f6674f346eaa1e201940d1b795c2799aea094cb5
Robert <61685814+Ungetoastet@users.noreply.github.com>
2020-08-03 11:41:47 +0200

    Update Readout.py

--- Readout.py
+++ Readout.py
@@ -4,8 +4,8 @@ import time
 print("Starting Readout")
 while True:
     try:
-        accel_data = Gyroscope.get_accel_data(Gyroscope)
-        gyro_data = Gyroscope.get_gyro_data(Gyroscope)
+        accel_data = Gyroscope.get_accel_data(self=Gyroscope)
+        gyro_data = Gyroscope.get_gyro_data(self=Gyroscope)
 
         print("Ax:{:.4f}\tAy:{:.4f}\tAz:{:.4f}\tGx:{:.4f}\tGy:{:.4f}\tGz:{:.4f} ".format(accel_data['x'], accel_data['y'], accel_data['z'], gyro_data['x'], gyro_data['y'], gyro_data['z']))
 
