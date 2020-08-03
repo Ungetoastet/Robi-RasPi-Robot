4b29beec39865562af9b8d13a6a647cd2cca7521
Robert <61685814+Ungetoastet@users.noreply.github.com>
2020-08-03 15:33:04 +0200

    Create ServoTest.py

---
+++ ServoTest.py
@@ -0,0 +1,15 @@
+from Gyro import Gyroscope
+import time
+from gpiozero import AngularServo
+
+gy = Gyroscope(0x68)
+servo0 = AngularServo(4, min_angle=-45, max_angle=45)
+
+print("Starting Gyro Hookup")
+while True:
+    try:
+        gyro_data = gy.get_gyro_data()
+        servo0.angle = gyro_data["x"]
+
+    except KeyboardInterrupt:
+        break
