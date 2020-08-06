2a1075f0a359dbe54b8547bff0f0dfe48077816b
Robert <61685814+Ungetoastet@users.noreply.github.com>
2020-08-06 15:20:34 +0200

    Update main.py
    added wiggle and shutdown

--- main.py
+++ main.py
@@ -11,23 +11,14 @@ for x in gpio:
     iteration += 1
 
 while True:
-    command = input()
+    command = input(">>")
     if command == "help":
         print("standby: Lay down")
         print("idle: Stand up")
+        print("wiggle: Stand up and shake")
+        print("shutdown: Lay down and quit")
 
-    if command == "idle":
-        servos[0].mid()
-        servos[1].mid()
-        servos[2].mid()
-        servos[3].mid()
-        servos[4].min()
-        servos[5].max()
-        servos[6].min()
-        servos[7].max()
-        time.sleep(1)
-    
-    if command == "standby":
+    elif command == "standby":
         servos[0].mid()
         servos[1].mid()
         servos[2].mid()
@@ -37,3 +28,54 @@ while True:
         servos[6].max()
         servos[7].min()
         time.sleep(1)
+
+    elif command == "idle":
+        servos[0].mid()
+        servos[1].mid()
+        servos[2].mid()
+        servos[3].mid()
+        servos[4].min()
+        servos[5].max()
+        servos[6].min()
+        servos[7].max()
+        time.sleep(1)
+
+    elif command == "wiggle":
+        servos[0].mid()
+        servos[1].mid()
+        servos[2].mid()
+        servos[3].mid()
+        servos[4].min()
+        servos[5].max()
+        servos[6].min()
+        servos[7].max()
+        active = True
+        while active:
+            try:
+                servos[0].mid()
+                servos[1].mid()
+                servos[2].mid()
+                servos[3].mid()
+                time.sleep(1)
+                servos[0].max()
+                servos[1].min()
+                servos[2].max()
+                servos[3].min()
+                time.sleep(1)
+            except KeyboardInterrupt:
+                active = False
+                
+    elif command == "shutdown":
+        servos[0].mid()
+        servos[1].mid()
+        servos[2].mid()
+        servos[3].mid()
+        servos[4].max()
+        servos[5].min()
+        servos[6].max()
+        servos[7].min()
+        time.sleep(1)
+        quit()
+    
+    else:
+        print("Command not found. Type 'help' for commands")
